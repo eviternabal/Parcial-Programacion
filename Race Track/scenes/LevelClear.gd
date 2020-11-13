587b851c14197af970be66a2cb258211e0b493ca
Megadraco <61173015+Megadraco@users.noreply.github.com>
2020-11-12 23:12:53 -0300

    Agregué funcionalidad a los demás botones de LevelClear.tscn, creé Globals.gd.

--- Race Track/scenes/LevelClear.gd
+++ Race Track/scenes/LevelClear.gd
@@ -7,7 +7,15 @@ var scene_lvlselect = load("res://scenes/level_select.tscn")
 func _on_lvlselector_pressed():
 	get_tree().change_scene_to(scene_lvlselect)
 
+func _on_Retry_pressed():
+	get_tree().change_scene(Globals.levels[Globals.current_level])
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _on_Next_level_pressed():
+	var next_scene = ""
+	Globals.current_level += 1
+	if Globals.current_level >= Globals.levels.size():
+		next_scene = "res://scenes/credits.tscn"
+	else:
+		next_scene = Globals.levels[Globals.current_level]
+	get_tree().change_scene(next_scene)
