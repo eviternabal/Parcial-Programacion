ca92c8ea7756c16ccc30c3d502c78d51187f2b9f
TotallyMadBro <juangarciajousset@gmail.com>
2020-11-12 22:27:27 -0300

    level selector y tema de botones

--- Race Track/scenes/menu.gd
+++ Race Track/scenes/menu.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 var scene_mainmenu = load("res://scenes/menu.tscn")
-var scene_lvlselect = load("res://scenes/test.tscn")
+var scene_lvlselect = load("res://scenes/level_select.tscn")
 var scene_credits = load("res://scenes/credits.tscn")
-
+var scene_lvl1 = load("res://scenes/test.tscn")
+var scene_lvl2 = load("res://scenes/test.tscn")
+var scene_lvl3 = load("res://scenes/test.tscn")
 
 func _on_lvlselector_pressed():
 	get_tree().change_scene_to(scene_lvlselect)
-
 
 func _on_credits_pressed():
 	get_tree().change_scene_to(scene_credits)
@@ -17,3 +18,11 @@ func _on_quit_pressed():
 
 func _on_back_pressed():
 	get_tree().change_scene_to(scene_mainmenu)
+
+func _on_lvl_pressed():
+	if $"lvl 1".pressed:
+		get_tree().change_scene_to(scene_lvl1)
+	if $"lvl 2".pressed:
+		get_tree().change_scene_to(scene_lvl2)
+	if $"lvl 3".pressed:
+		get_tree().change_scene_to(scene_lvl3)
