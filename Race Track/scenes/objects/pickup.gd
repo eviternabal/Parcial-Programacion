75674c18e283946b619527fb18d7e605783568ad
Neokroup <nicolas@kroupensky.com.ar>
2020-11-18 00:39:13 -0300

    Sanaoria asheglaa

--- Race Track/scenes/objects/pickup.gd
+++ Race Track/scenes/objects/pickup.gd
@@ -2,10 +2,13 @@ extends Position2D
 
 signal pickup_pickuped
 
+onready var animation = get_node("AnimationPlayer")
+
 func _ready():
 	pass 
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("wheels"):
 		emit_signal("pickup_pickuped")
+		animation.play("ded")
 	pass 
